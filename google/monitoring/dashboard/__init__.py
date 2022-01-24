98943a82f22db83c938b979893824f5565c5c7f8
Anthonios Partheniou <partheniou@google.com>
2022-01-24 15:52:59 +0000

    remove hardcoded date

--- google/monitoring/dashboard/__init__.py
+++ google/monitoring/dashboard/__init__.py
@@ -88,7 +88,7 @@ __all__ = (
 import_warning_message = (
     "The client in the `google.monitoring.dashboard` namespace is no longer updated. "
     "Please use the client in namespace `google.cloud.monitoring_dashboard` instead. "
-    "After June 1, 2022, importing code from the `google.monitoring.dashboard` namespace "
+    "In a future release, importing code from the `google.monitoring.dashboard` namespace "
     "may result in a RuntimeError. If you need to continue to use `google.monitoring.dashboard` "
     "after this date, please pin to a specific version of 'google-cloud-monitoring-dashboards'. "
     "If you have questions, please file an issue: "
